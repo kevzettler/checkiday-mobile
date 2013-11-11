4ceb64f34b95945a150e5b4a430cdd2de0a89303
Kev zettler <kevzettler@gmail.com>
2013-11-10 18:25:20 -0800

    ITS ALIVE some kind of grunt build system and some more style tweaks

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,13 +4,48 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
 
+    trigger_config: grunt.file.readJSON('local_config.json'),
+
+    trigger_creds: grunt.file.readJSON('trigger_creds.json'),
+
+    clean: {
+      main:{
+        src: "dist"
+      }
+    },
+
+    copy:{
+      main:{
+        expand: true,
+        cwd: 'app/',
+        src: ['**'],
+        dest: 'dist/'
+      }
+    },
+
     uglify: {
       options: {
         banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
       },
-      build: {
-        src: 'src/<%= pkg.name %>.js',
-        dest: 'build/<%= pkg.name %>.min.js'
+      my_target: {
+        files:{
+        'dist/js/main.min.js' :[
+           "app/js/vendor/zepto.js",
+           "app/js/vendor/zepto.zelector.js",
+           "app/js/vendor/zepto.fn.slide.js",
+           "app/js/vendor/foundation.min.js",
+           "app/js/hardcache.js",
+           "app/js/vendor/jquery.calendario.js",
+           "app/js/main.js"
+        ]}
+      }
+    },
+
+    cssmin:{
+      combine:{
+        files:{
+          'dist/css/main.min.css': ['app/css/*.css']
+        }
       }
     },
 
@@ -26,31 +61,48 @@ module.exports = function(grunt) {
 
     preprocess : {
       dev : {
-        src : './src/index.html',
-        dest : './dist/index.html'
+        src : 'app/index.html',
+        dest : 'dist/index.html'
       },
 
       prod : {
-        src : './src/index.html',
-        dest : './dist/<%= pkg.version %>/index.html'
+        src : 'app/index.html',
+        dest : 'dist/index.html'
       }
+    },
+
+    forge: {
+      ios_build: {
+        args: ['build', 'ios'],
+        options: {
+          username: '<%= trigger_creds.username %>',
+          password: '<%= trigger_creds.password %>'
+        }
+      },
+      ios_sim: { args: ['run', 'ios'] },
+      ios_device: { args: ['run', 'ios', '--ios.device', 'device'] },
+      ios_package: { args: ['package', 'ios'] }
     }
+
   });
 
   // Load the plugins
+  grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-uglify');
-
   grunt.loadNpmTasks('grunt-forge');
-
   grunt.loadNpmTasks('grunt-preprocess');
-
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-env');
+  grunt.loadNpmTasks('grunt-contrib-copy');
 
   // Register task(s).
-  grunt.registerTask('default', ['jshint']);
+  grunt.registerTask('default', ['dev']);
+  grunt.registerTask('dev', ['env:dev', 'clean', 'copy', 'cssmin', 'uglify', 'preprocess:dev', 'sim']);
+  grunt.registerTask('prod', ['env:prod', 'clean', 'cssmin', 'uglify', 'preprocess:prod']);
 
-  grunt.registerTask('dev', ['jshint', 'env:dev', 'clean:dev', 'preprocess:dev']);
-
-  grunt.registerTask('prod', ['jshint', 'env:prod', 'clean:prod', 'uglify:prod', 'cssmin:prod', 'copy:prod', 'preprocess:prod']);
+  grunt.registerTask("build", ['forge:ios_build']);
+  grunt.registerTask("sim", ['build', 'forge:ios_sim']);
+  grunt.registerTask("device", ['build', 'forge:ios_device']);
+  grunt.registerTask("package", ['build', 'forge:ios_package']);
 
 };
